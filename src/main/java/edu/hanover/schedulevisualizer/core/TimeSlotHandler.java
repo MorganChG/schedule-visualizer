e1508079030bee523b935ca5ad021f5b29e3bb5b
MorganChG <gatliffmorgan6@gmail.com>
2024-09-23 15:33:00 -0400

    Replace getFirsti with get(0)

--- src/main/java/edu/hanover/schedulevisualizer/core/TimeSlotHandler.java
+++ src/main/java/edu/hanover/schedulevisualizer/core/TimeSlotHandler.java
@@ -6,8 +6,6 @@ import edu.hanover.schedulevisualizer.core.entity.Weekday;
 
 import java.util.List;
 
-import static edu.hanover.schedulevisualizer.core.entity.Weekday.TR;
-
 class TimeSlotHandler extends TimeSlotCombiner<TimeSlot> {
     EntityFactory ef;
     public TimeSlotHandler(EntityFactory ef) {
@@ -21,10 +19,10 @@ class TimeSlotHandler extends TimeSlotCombiner<TimeSlot> {
 
     @Override
     protected TimeSlot combineWithBothAssigned(List<Weekday> wkd1, Integer sn1, List<Weekday> wkd2, Integer sn2) {
-        if (wkd2.getFirst().isTR() && wkd1.getFirst().isTR()) {
+        if (wkd2.get(0).isTR()) if (wkd1.get(0).isTR()) {
             return ef.makeHCTimeSlot(wkd1, sn2);
         }
-        if (!wkd2.getFirst().isTR() && !wkd1.getFirst().isTR()) {
+        if (!wkd2.get(0).isTR()) if (!wkd1.get(0).isTR()) {
             return ef.makeHCTimeSlot(wkd1, sn2);
         }
         return ef.makeHCTimeSlot(wkd2, sn2);
@@ -32,9 +30,10 @@ class TimeSlotHandler extends TimeSlotCombiner<TimeSlot> {
 
     @Override
     protected TimeSlot combineWithFirstUnassignedSecondAssigned(List<Weekday> weekdays2, Integer slotnum2) {
-        if (weekdays2.getFirst().isTR()) {
+        if (weekdays2.get(0).isTR()) {
             weekdays2 = Weekday.TR();
-        } if(!weekdays2.getFirst().isTR()) {
+        }
+        if(!weekdays2.get(0).isTR()) {
             weekdays2 = Weekday.MWF();
         }
         return ef.makeHCTimeSlot(weekdays2, slotnum2);
